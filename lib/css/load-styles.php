c827cbbee2c1c04ca3d3b432928299f64362dd2e
msgimanila <msgimanila@yahoo.com>
2016-12-04 05:23:24 +0800

    Add files via upload

--- lib/css/load-styles.php
+++ lib/css/load-styles.php
@@ -11,5 +11,5 @@ function hasloo_load_styles() {
 
 add_action('admin_init', 'hasloo_load_admin_styles');
 function hasloo_load_admin_styles() {
-	wp_enqueue_style('hasloo_admin_css', GENESIS_CSS_URL.'/admin.css');
+	wp_enqueue_style('hasloo_admin_css', HASLOO_CSS_URL.'/admin.css');
 }
